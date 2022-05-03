7e2962916c45bcc13654315b94d27ee710b37edf
user <sivtsovatanya1985@gmail.com>
2022-05-03 11:50:29 +0300

    Updated project:
    - added functionality for adding a new employee using the input form

--- src/employees-add-form/employees-add-form.js
+++ src/employees-add-form/employees-add-form.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import './employees-add-form.css';
 
 const EmployeesAddForm = () => {
+    const [name, setName] = useState('');
+    const [salary, setSalary] = useState(0);
+
+    const nameChange = (event) => {
+        setName(event.target.value)
+    }
+
+    const salaryChange = (event) => {
+        setSalary(event.target.value)
+    }
+
     return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form
                 className="add-form d-flex">
                 <input type="text"
+                       value={name}
+                       onChange={nameChange}
                        className="form-control new-post-label"
                        placeholder="Как его зовут?" />
                 <input type="number"
+                       value={salary}
+                       onChange={salaryChange}
                        className="form-control new-post-label"
                        placeholder="З/П в $?" />
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
             </form>
+            <div>name {name} salary {salary}</div>
         </div>
     )
 }
