a7b14af8bfe18b60fd05b29180f46e9d4d183fec
user <sivtsovatanya1985@gmail.com>
2022-05-03 13:13:43 +0300

    Updated project:
    - added functionality for deleting items;

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,12 +6,26 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './App.css';
 
+
+
 const App = () => {
-    const data = [
-        {name: 'Tetiana Si', salary: 2500, increase: false, rise: true, id: 1},
-        {name: 'Natalia Si', salary: 1200, increase: true, rise: false, id: 2},
-        {name: 'Galina G', salary: 3650, increase: false, rise: false, id: 3},
-    ]
+    const [data, setData] = useState(
+        [
+            {name: 'Tetiana Si', salary: 2500, increase: false, rise: true, id: 1},
+            {name: 'Natalia Si', salary: 1200, increase: true, rise: false, id: 2},
+            {name: 'Galina G', salary: 3650, increase: false, rise: false, id: 3},
+        ]
+    );
+   const deleteItem = (id) => {
+       const index = data.findIndex(elem => elem.id === id);
+       const before = data.slice(0, index);
+       const after = data.slice(index + 1);
+       const newArr = [...before, ...after]
+       setData([...newArr]);
+   }
+
+
+
 
     return (
         <div className="app">
@@ -22,7 +36,9 @@ const App = () => {
                 <AppFilter/>
             </div>
 
-            <EmployeesList data={data}/>
+            <EmployeesList data={data}
+                           onDelete={deleteItem}
+            />
             <EmployeesAddForm/>
         </div>
     );
